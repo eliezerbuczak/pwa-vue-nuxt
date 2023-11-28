955faf64a95206c81cb7cce6a60c3d9204f7fe5d
eliezer <eliezerbuczak@gmail.com>
2023-11-28 10:07:05 -0300

    router dinamic

---
+++ pages/auth/[id].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Com id</h1>
+
+    <h1>{{route.params.id}}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
